0f5bf736a20970f28e417b28600875b6f8b199a5
twisted_pear <twisted_pear@twstd.neomailbox.ch>
2014-03-25 17:53:51 +0100

    More spongewrap tests

--- testSpongeWrap.c
+++ testSpongeWrap.c
@@ -6,29 +6,78 @@
 #include "KeccakPad_10_1.h"
 #include "spongewrap.h"
 
-static int testEmpty(spongewrap *w_enc, spongewrap *w_dec)
+#define T_LEN_MAX 512
+
+#define B_LEN_MAX 512
+#define B_PATTERN 0xFE
+
+static int testRunner(permutation *f, pad *p, size_t rate, size_t block_size,
+		const unsigned char *key, const size_t key_byte_len,
+		int (*test)(spongewrap*, spongewrap*))
 {
-	if (spongewrap_wrap(w_enc, NULL, 0, NULL, 0, NULL, NULL, 0) != 0) {
-		return 1;
+	int ret = 1;
+
+	spongewrap *w_enc = spongewrap_init(f, p, rate, block_size, key, key_byte_len);
+	if (w_enc == NULL) {
+		goto enc_fail;
 	}
 
-	if (spongewrap_unwrap(w_dec, NULL, 0, NULL, 0, NULL, 0, NULL) != 0) {
-		return 1;
+	spongewrap *w_dec = spongewrap_init(f, p, rate, block_size, key, key_byte_len);
+	if (w_dec == NULL) {
+		goto dec_fail;
+	}
+
+
+	ret = test(w_enc, w_dec);
+
+	spongewrap_free(w_dec);
+dec_fail:
+	spongewrap_free(w_enc);
+enc_fail:
+
+	return ret;
+}
+
+static int testEmpty_a_b(spongewrap *w_enc, spongewrap *w_dec)
+{
+	unsigned char t[T_LEN_MAX];
+
+	size_t t_len;
+	for (t_len = 0; t_len <= T_LEN_MAX; t_len++) {
+		if (spongewrap_wrap(w_enc, NULL, 0, NULL, 0, NULL, t, t_len) != 0) {
+			return 1;
+		}
+
+		if (spongewrap_unwrap(w_dec, NULL, 0, NULL, 0, t, t_len, NULL) != 0) {
+			return 1;
+		}
 	}
 
 	return 0;
 }
 
-static int testEmpty_a_b(spongewrap *w_enc, spongewrap *w_dec)
+static int testEncDec_empty_a(spongewrap *w_enc, spongewrap *w_dec)
 {
-	unsigned char t[2];
+	unsigned char b[B_LEN_MAX];
+	unsigned char c[B_LEN_MAX];
+	unsigned char d[B_LEN_MAX];
+	unsigned char t[T_LEN_MAX];
 
-	if (spongewrap_wrap(w_enc, NULL, 0, NULL, 0, NULL, t, 2) != 0) {
-		return 1;
-	}
+	memset(b, B_PATTERN, B_LEN_MAX);
 
-	if (spongewrap_unwrap(w_dec, NULL, 0, NULL, 0, t, 2, NULL) != 0) {
-		return 1;
+	size_t b_len;
+	for (b_len = 0; b_len <= B_LEN_MAX; b_len++) {
+		if (spongewrap_wrap(w_enc, NULL, 0, b, b_len, c, t, T_LEN_MAX) != 0) {
+			return 1;
+		}
+
+		if (spongewrap_unwrap(w_dec, NULL, 0, c, b_len, t, T_LEN_MAX, d) != 0) {
+			return 1;
+		}
+
+		if (memcmp(b, d, b_len) != 0) {
+			return 1;
+		}
 	}
 
 	return 0;
@@ -49,36 +98,22 @@ static int testSpongeWrap_internal(size_t rate, size_t block_size,
 		goto pad_fail;
 	}
 
-	spongewrap *w_enc = spongewrap_init(f, p, rate, block_size, key, key_byte_len);
-	if (w_enc == NULL) {
-		goto enc_fail;
-	}
-
-	spongewrap *w_dec = spongewrap_init(f, p, rate, block_size, key, key_byte_len);
-	if (w_dec == NULL) {
-		goto dec_fail;
-	}
-
-	if (testEmpty(w_enc, w_dec) != 0) {
+	if (testRunner(f, p, rate, block_size, key, key_byte_len, testEmpty_a_b) != 0) {
 		goto fail;
 	}
 
-	if (testEmpty_a_b(w_enc, w_dec) != 0) {
+	if (testRunner(f, p, rate, block_size, key, key_byte_len, testEncDec_empty_a) != 0) {
 		goto fail;
 	}
 
 	ret = 0;
 
 fail:
-	spongewrap_free(w_dec);
-dec_fail:
-	spongewrap_free(w_enc);
-enc_fail:
 	keccakPad_10_1_free(p);
 pad_fail:
 	keccakF_1600_free(f);
-
 f_fail:
+
 	return ret;
 }
 
