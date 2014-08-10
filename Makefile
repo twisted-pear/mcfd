fc636d1ac4c616df7b48301f7bf0a70d9e06c228
twisted_pear <twisted_pear@twstd.neomailbox.ch>
2014-08-10 23:52:20 +0200

    Add simple program that uses SpongePRG
    Just initializes a SpongePRG with a given seed and writes some generated
    bytes to a file.

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 TEST_BIN= keccak_test
 MCFD_BIN= mcfd
-BINS	= $(TEST_BIN) $(MCFD_BIN)
+RNG_BIN	= keccak_rng
+BINS	= $(TEST_BIN) $(MCFD_BIN) $(RNG_BIN)
 
 TEST_IN	= LongMsgKAT.txt MonteCarlo.txt ShortMsgKAT.txt
 TEST_OUT= $(foreach type,LongMsgKAT MonteCarlo ShortMsgKAT,$(foreach size,224 256 384 512, $(type)_$(size).txt)) TestDuplex.txt
@@ -10,20 +11,23 @@ LIBS	=
 
 S_TEST	= main.c genKAT.c KeccakNISTInterface.c testDuplex.c testSpongeWrap.c testSpongePRG.c
 S_MCFD	= mcfd_auth.c mcfd_common.c mcfd_crypto.c mcfd_main.c mcfd_net.c 
+S_RNG	= keccak_rng.c
 S_COMM	= sponge.c duplex.c crypto_helpers.c spongewrap.c spongeprg.c
 S_PERM	= KeccakF-1600.c 
 S_PAD	= KeccakPad_10_1.c 
 
 O_TEST	= $(S_TEST:.c=.o)
 O_MCFD	= $(S_MCFD:.c=.o)
+O_RNG	= $(S_RNG:.c=.o)
 O_COMM	= $(S_COMM:.c=.o)
 O_PERM	= $(S_PERM:.c=.o)
 O_PAD	= $(S_PAD:.c=.o)
 
 OS_TEST	= $(O_COMM) $(O_PERM) $(O_PAD) $(O_TEST)
 OS_MCFD	= $(O_COMM) $(O_PERM) $(O_PAD) $(O_MCFD)
+OS_RNG	= $(O_COMM) $(O_PERM) $(O_PAD) $(O_RNG)
 
-OBJS	= $(O_COMM) $(O_PERM) $(O_PAD) $(O_TEST) $(O_MCFD)
+OBJS	= $(O_COMM) $(O_PERM) $(O_PAD) $(O_TEST) $(O_MCFD) $(O_RNG)
 
 CFLAGS	= -Wall -pedantic -std=c99 -D_XOPEN_SOURCE=500 -g
 LDFLAGS	=
@@ -45,6 +49,9 @@ $(TEST_BIN): $(OS_TEST)
 
 $(MCFD_BIN): $(OS_MCFD)
 	$(CC) $(LDFLAGS) -o $@ $(OS_MCFD) $(LIBS)
+
+$(RNG_BIN): $(OS_RNG)
+	$(CC) $(LDFLAGS) -o $@ $(OS_RNG) $(LIBS)
 
 $(O_PERM): $(S_PERM) permutation.h
 	$(CC) $(CFLAGS) -o $@ -c $<
---
+++ keccak_rng.c
@@ -0,0 +1,138 @@
+#include <errno.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "KeccakF-1600.h"
+#include "KeccakPad_10_1.h"
+#include "spongeprg.h"
+
+static const char *progname = "keccak_rng";
+
+static void usage(void)
+{
+	fprintf(stderr, "Usage: %s <seed> <outfile> [<nbytes>]\n", progname);
+	exit(EXIT_FAILURE);
+}
+
+static unsigned char buf[1024];
+
+int main(int argc, char *const *argv)
+{
+	int ret = EXIT_FAILURE;
+
+	if (argc < 3 || argc > 4) {
+		usage();
+	}
+
+	if (argv[0] == NULL) {
+		usage();
+	}
+
+	progname = argv[0];
+
+	char *seed = argv[1];
+	char *outfile = argv[2];
+
+	if (seed == NULL || outfile == NULL) {
+		usage();
+	}
+
+	long long length = 0;
+
+	if (argc == 4) {
+		char *nbytes = argv[3];
+		if (nbytes == NULL) {
+			usage();
+		}
+
+		length = strtoll(nbytes, NULL, 10);
+		if (length < 0 || errno != 0) {
+			usage();
+		}
+	}
+
+	FILE *outf = fopen(outfile, "wb");
+	if (outf == NULL) {
+		fprintf(stderr, "ERROR: Failed to open outfile.\n");
+		return EXIT_FAILURE;
+	}
+
+	permutation *f = keccakF_1600_init(1026);
+	if (f == NULL) {
+		fprintf(stderr, "ERROR: Failed to create permutation.\n");
+		fclose(outf);
+		return EXIT_FAILURE;
+	}
+
+	pad *p = keccakPad_10_1_init(1026);
+	if (p == NULL) {
+		fprintf(stderr, "ERROR: Failed to create pad.\n");
+		fclose(outf);
+		keccakF_1600_free(f);
+		return EXIT_FAILURE;
+	}
+
+	spongeprg *g = spongeprg_init(f, p, 1026, 128);
+	if (g == NULL) {
+		fprintf(stderr, "ERROR: Failed to create RNG.\n");
+		fclose(outf);
+		keccakPad_10_1_free(p);
+		keccakF_1600_free(f);
+		return EXIT_FAILURE;
+	}
+
+	if (spongeprg_feed(g, (unsigned char *) seed, strlen(seed)) != 0) {
+		fprintf(stderr, "ERROR: Failed to seed PRG.\n");
+		goto out;
+	}
+
+	if (length == 0) {
+		for (;;) {
+			if (spongeprg_fetch(g, buf, sizeof(buf)) != 0) {
+				fprintf(stderr, "ERROR: Fetch failed.\n");
+				goto out;
+			}
+
+			if (fwrite(buf, 1, sizeof(buf), outf) != sizeof(buf)) {
+				fprintf(stderr, "ERROR: Failed to write.\n");
+				goto out;
+			}
+		}
+	}
+
+	while (length > sizeof(buf)) {
+		if (spongeprg_fetch(g, buf, sizeof(buf)) != 0) {
+			fprintf(stderr, "ERROR: Fetch failed.\n");
+			goto out;
+		}
+
+		if (fwrite(buf, 1, sizeof(buf), outf) != sizeof(buf)) {
+			fprintf(stderr, "ERROR: Failed to write.\n");
+			goto out;
+		}
+
+		length -= sizeof(buf);
+	}
+
+	if (spongeprg_fetch(g, buf, length) != 0) {
+		fprintf(stderr, "ERROR: Fetch failed.\n");
+		goto out;
+	}
+
+	if (fwrite(buf, 1, length, outf) != length) {
+		fprintf(stderr, "ERROR: Failed to write.\n");
+		goto out;
+	}
+
+	ret = EXIT_SUCCESS;
+
+out:
+	fclose(outf);
+	spongeprg_free(g);
+	keccakPad_10_1_free(p);
+	keccakF_1600_free(f);
+
+	return ret;
+}
