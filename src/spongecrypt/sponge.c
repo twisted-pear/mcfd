eed3a61277a470bad1f293f9ad8a160a9b512ccf
twisted_pear <twisted_pear@twstd.neomailbox.ch>
2014-08-23 20:47:35 +0200

    Change sponge to work without intermediate buffers

--- src/spongecrypt/sponge.c
+++ src/spongecrypt/sponge.c
@@ -7,9 +7,7 @@
 #include "crypto_helpers.h"
 
 struct internals {
-	unsigned char *remaining;
 	size_t remaining_bits;
-	size_t width;
 	enum { PHASE_ABSORB = 0, PHASE_SQUEEZE } phase;
 };
 
@@ -55,16 +53,7 @@ sponge *sponge_init(permutation *f, pad *p, const size_t rate)
 
 	struct internals *internal = (struct internals *) sp->internal;
 
-	internal->remaining = calloc(rate / 8, 1);
-	if (internal->remaining == NULL) {
-		free(internal);
-		free(sp);
-		return NULL;
-	}
-
 	internal->remaining_bits = 0;
-
-	internal->width = f->width;
 
 	internal->phase = PHASE_ABSORB;
 
@@ -77,9 +66,6 @@ void sponge_free(sponge *sp)
 	assert(sp->internal != NULL);
 
 	struct internals *internal = (struct internals *) sp->internal;
-
-	explicit_bzero(internal->remaining, sp->rate / 8);
-	free(internal->remaining);
 
 	internal->remaining_bits = 0;
 
@@ -102,40 +88,54 @@ int sponge_absorb(sponge *sp, const unsigned char *input, const size_t input_bit
 	}
 
 	/* Don't allow absorbs with partial bytes, except at the end.
-	 * Rationale: The reference implementation doesn't allow this either (and it's easier). */
+	 * Rationale: The reference implementation doesn't allow this either (and it's
+	 * easier). */
 	if (internal->remaining_bits % 8 != 0) {
 		return 1;
 	}
 
-	/* Finish absorbing any started block from a previous sponge_absorb. */
-	size_t bits_needed = 0;
-	if (internal->remaining_bits != 0) {
-		bits_needed = sp->rate - internal->remaining_bits;
-		memcpy(internal->remaining + internal->remaining_bits / 8, input, bits_needed / 8);
+	size_t in_remaining = input_bit_len;
+	const unsigned char *in_cur = input;
 
-		if (sp->f->xor(sp->f, 0, internal->remaining, sp->rate) != 0) {
+	/* Finish absorbing any started block from a previous sponge_absorb. */
+	size_t bits_to_block = sp->rate - internal->remaining_bits;
+	if (in_remaining >= bits_to_block && internal->remaining_bits != 0) {
+		assert(bits_to_block % 8 == 0);
+
+		if (sp->f->xor(sp->f, internal->remaining_bits, in_cur,
+					bits_to_block) != 0) {
 			assert(0);
 		}
 		sp->f->f(sp->f);
+
+		internal->remaining_bits = 0;
+
+		in_remaining -= bits_to_block;
+		in_cur += bits_to_block / 8;
 	}
 
 	/* Absorb full blocks in the input. */
-	size_t remaining_bit_len = input_bit_len - bits_needed;
-	const unsigned char *begin_full = input + bits_needed / 8;
+	while (in_remaining >= sp->rate) {
+		assert(internal->remaining_bits == 0);
 
-	while (remaining_bit_len >= sp->rate) {
-		if (sp->f->xor(sp->f, 0, begin_full, sp->rate) != 0) {
+		if (sp->f->xor(sp->f, 0, in_cur, sp->rate) != 0) {
 			assert(0);
 		}
 		sp->f->f(sp->f);
 
-		remaining_bit_len -= sp->rate;
-		begin_full += sp->rate / 8;
+		in_remaining -= sp->rate;
+		in_cur += sp->rate / 8;
 	}
 
-	/* Copy remainder into sp->internal for further use. */
-	internal->remaining_bits = remaining_bit_len;
-	memcpy(internal->remaining, begin_full, (remaining_bit_len + 7) / 8);
+	/* Xor in the remainder but don't permute yet. */
+	if (sp->f->xor(sp->f, internal->remaining_bits, in_cur, in_remaining) != 0) {
+		assert(0);
+	}
+
+	internal->remaining_bits += in_remaining;
+
+	assert(internal->remaining_bits < sp->rate);
+	assert(internal->phase == PHASE_ABSORB);
 
 	return 0;
 }
@@ -146,24 +146,26 @@ int sponge_absorb_final(sponge *sp)
 
 	struct internals *internal = (struct internals *) sp->internal;
 
+	assert(internal->remaining_bits < sp->rate);
+
 	/* Only absorb in absorb phase. */
 	if (internal->phase != PHASE_ABSORB) {
 		return 1;
 	}
 
-	/* Apply padding and add last block. */
-	if (sp->f->xor(sp->f, 0, internal->remaining, internal->remaining_bits) != 0) {
-		assert(0);
-	}
+	/* Apply padding and permute last block(s). */
 	if (sp->p->pf(sp->p, sp->f, internal->remaining_bits) != 0) {
 		assert(0);
 	}
 
-	explicit_bzero(internal->remaining, sp->rate / 8);
-	internal->remaining_bits = 0;
+	internal->remaining_bits = sp->rate;
 
 	/* Switch to squeezing. */
 	internal->phase = PHASE_SQUEEZE;
+
+	assert(internal->remaining_bits <= sp->rate);
+	assert(internal->remaining_bits % 8 == 0);
+	assert(internal->remaining_bits != 0);
 
 	return 0;
 }
@@ -179,59 +181,62 @@ int sponge_squeeze(sponge *sp, unsigned char *output, const size_t output_bit_le
 		return 1;
 	}
 
+	assert(internal->remaining_bits <= sp->rate);
 	assert(internal->remaining_bits % 8 == 0);
+	assert(internal->remaining_bits != 0);
 
 	/* Don't allow squeezing partial bytes.
-	 * Rationale: The reference implementation doesn't allow this either (and it's easier). */
+	 * Rationale: The reference implementation doesn't allow this either (and it's
+	 * easier). */
 	if (output_bit_len % 8 != 0) {
 		return 1;
 	}
 
-	size_t bits_needed = output_bit_len;
-	unsigned char *current_out = output;
+	size_t out_remaining = output_bit_len;
+	unsigned char *out_cur = output;
 
 	/* Hand out old data first. */
-	if (internal->remaining_bits != 0) {
-		assert(internal->remaining_bits < sp->rate);
-
-		unsigned char *remaining_start = internal->remaining + (sp->rate -
-			internal->remaining_bits) / 8;
-		if (internal->remaining_bits >= output_bit_len) {
-			memcpy(output, remaining_start, output_bit_len / 8);
-			internal->remaining_bits -= output_bit_len;
-			return 0;
-		} else {
-			memcpy(output, remaining_start, internal->remaining_bits / 8);
-			bits_needed -= internal->remaining_bits;
-			current_out += internal->remaining_bits / 8;
-			internal->remaining_bits = 0;
-		}
-	}
-
-	assert(internal->remaining_bits == 0);
-	assert(bits_needed > 0);
-
-	while (bits_needed > sp->rate) {
-		/* This works because (rate % 8 == 0). */
-		if(sp->f->get(sp->f, 0, current_out, sp->rate) != 0) {
+	if (internal->remaining_bits <= out_remaining) {
+		size_t remaining_start = sp->rate - internal->remaining_bits;
+		if(sp->f->get(sp->f, remaining_start, out_cur,
+					internal->remaining_bits) != 0) {
 			assert(0);
 		}
 		sp->f->f(sp->f);
 
-		bits_needed -= sp->rate;
-		current_out += sp->rate / 8;
+		out_remaining -= internal->remaining_bits;
+		out_cur += internal->remaining_bits / 8;
+
+		internal->remaining_bits = sp->rate;
 	}
 
-	if (sp->f->get(sp->f, 0, internal->remaining, sp->rate) != 0) {
+	/* Get as many full blocks as we need. */
+	while (out_remaining >= sp->rate) {
+		assert(internal->remaining_bits == sp->rate);
+
+		if (sp->f->get(sp->f, 0, out_cur, sp->rate) != 0) {
+			assert(0);
+		}
+		sp->f->f(sp->f);
+
+		out_remaining -= sp->rate;
+		out_cur += sp->rate / 8;
+	}
+
+	assert(out_remaining < internal->remaining_bits);
+	assert(out_remaining < sp->rate);
+
+	size_t remaining_start = sp->rate - internal->remaining_bits;
+	if (sp->f->get(sp->f, remaining_start, out_cur, out_remaining) != 0) {
 		assert(0);
 	}
-	sp->f->f(sp->f);
 
-	assert(bits_needed % 8 == 0);
-	assert(bits_needed <= sp->rate);
+	internal->remaining_bits -= out_remaining;
 
-	memcpy(current_out, internal->remaining, bits_needed / 8);
-	internal->remaining_bits = sp->rate - bits_needed;
+	assert(internal->remaining_bits <= sp->rate);
+	assert(internal->remaining_bits % 8 == 0);
+	assert(internal->remaining_bits != 0);
+	assert(internal->phase == PHASE_SQUEEZE);
 
 	return 0;
 }
